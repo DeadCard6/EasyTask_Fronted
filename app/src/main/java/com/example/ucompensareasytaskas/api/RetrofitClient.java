b2f7fd7e369a1db54bd04a6d221a11df8f5f7340
Juan Herrera <jp715224@gmail.com>
2024-10-23 16:33:45 -0500

    Login

--- app/src/main/java/com/example/ucompensareasytaskas/api/RetrofitClient.java
+++ app/src/main/java/com/example/ucompensareasytaskas/api/RetrofitClient.java
@@ -1,11 +1,14 @@
 package com.example.ucompensareasytaskas.api;
 
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://192.168.1.23:8080/";
+    private static final String BASE_URL = "http://192.168.1.23:8080";
     private static Retrofit retrofit = null;
 
     // Singleton para obtener una instancia de Retrofit
@@ -21,7 +24,22 @@ public class RetrofitClient {
 
     // Método para obtener la instancia de ApiService
     public static ApiService getApiService() {
-        return getClient().create(ApiService.class);
+        if (retrofit == null) {
+            // Configuramos OkHttpClient con los timeout personalizados
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .connectTimeout(30, TimeUnit.SECONDS)  // Timeout para conexión
+                    .readTimeout(30, TimeUnit.SECONDS)     // Timeout para leer datos
+                    .writeTimeout(30, TimeUnit.SECONDS)    // Timeout para escribir datos
+                    .build();
+
+            // Configuramos Retrofit con el cliente OkHttp personalizado
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)  // Usamos el cliente con timeout personalizado
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit.create(ApiService.class);
     }
 
 }
